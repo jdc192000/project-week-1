95735053ff444cb2ca55c02623c9daface2a8ff9
John Coleman <johncoleman_2@hotmail.com>
2018-03-01 01:44:22 -0600

    initial load

---
+++ assets/js/firebase/firebase.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAjkKxHwrGCjbC7co4BBzmMiNNfVhvjY-M",
+    authDomain: "project-one-d470f.firebaseapp.com",
+    databaseURL: "https://project-one-d470f.firebaseio.com",
+    projectId: "project-one-d470f",
+    storageBucket: "",
+    messagingSenderId: "498815701290"
+  };
+  firebase.initializeApp(config);
