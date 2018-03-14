bf42d8f4762df503c1d3102c9845ba5a66d03b27
John Coleman <johncoleman_2@hotmail.com>
2018-03-14 18:34:13 -0500

    presentation

--- assets/js/firebase/firebase.js
+++ assets/js/firebase/firebase.js
@@ -1,13 +1,80 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAjkKxHwrGCjbC7co4BBzmMiNNfVhvjY-M",
-    authDomain: "project-one-d470f.firebaseapp.com",
-    databaseURL: "https://project-one-d470f.firebaseio.com",
-    projectId: "project-one-d470f",
-    storageBucket: "",
-    messagingSenderId: "498815701290"
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAjkKxHwrGCjbC7co4BBzmMiNNfVhvjY-M",
+  authDomain: "project-one-d470f.firebaseapp.com",
+  databaseURL: "https://project-one-d470f.firebaseio.com",
+  projectId: "project-one-d470f",
+  storageBucket: "project-one-d470f.appspot.com",
+  messagingSenderId: "498815701290"
+};
+
+firebase.initializeApp(config);
+
+var wishList = [];
+
+function retrieveWishlist() {
+
+  database.ref(userKey).once('value').then(function (snapshot) {
+    console.log(snapshot.val());
+
+    if (snapshot.hasChild("list")) {
+
+      userKey = snapshot.val().list;
+      console.log("retrieve list:  " + userKey);
+      wishList = snapshot.val().list;
+
+    } else {
+      userKey = userID
+
+      console.log(userKey);
+
+    };
+
+  });
+};
+
+function writeWishlist() {
+
+  var dataFormat = {
+    parent: parentKey,
+    list: wishList
   };
-  firebase.initializeApp(config);
 
+  database.ref().child(userKey).set(dataFormat);
 
-  
+};
+
+// function readParent() {
+
+//   database.ref(parentKey).once('value').then(function (snapshot) {
+//     console.log(snapshot.val());
+
+//     userKey = snapshot.val().child;
+
+//   });
+// };
+
+function createNewChild() {
+
+  var dataFormat = {
+    parent: parentKey
+  }
+
+  database.ref().child(userKey).set(dataFormat);
+
+};
+
+function parentOrchild() {
+  database.ref(userID).once('value').then(function (snapshot) {
+    console.log(snapshot.val());
+
+    if (snapshot.hasChild("child")) {
+      userKey = snapshot.val().child;
+    } else {
+      userKey = userID
+    };
+
+    retrieveData()
+  });
+
+};
